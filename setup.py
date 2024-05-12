7c60f63777b1fc288a72517a5886f992b6eb1d01
alan <al6nlee@gmail.com>
2024-05-12 21:04:44 +0800

    Update version 0.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup, Command
 from shutil import rmtree
 
 NAME = 'loggingA'
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 
 here = os.path.abspath(os.path.dirname(__file__))
 
